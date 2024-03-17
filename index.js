6d8af26a93ede47d0cee8afe1ab61a7719ffa135
LWSE13 <leewheeler888@gmail.com>
2024-03-17 18:40:29 +0000

    added questions, added require functions for modules begun work on readme generation

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please list the collaborators if any for your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init  = () => {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = 
+        `
+        # ${answers.title}
+        
+        ## Description
+
+        ## Installation
+
+        ## Usage
+
+        ## Credits
+
+        ## License
+        `
+    })
+}
 
 // Function call to initialize app
 init();
